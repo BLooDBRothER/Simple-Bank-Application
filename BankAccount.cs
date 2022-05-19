0b5936b20b2e7d9987527dd13a1cd6774199624f
BLooDBRothER <computertechnology446@gmail.com>
2022-05-19 11:57:38 +0530

    Add deposit and withdraw with tests

--- BankAccount.cs
+++ BankAccount.cs
@@ -1,20 +1,50 @@
 namespace BankApplication;
 
-public class BankAccount{
+public class BankAccount
+{
     private static int accountNumberSeed = 1234567890;
-    public string accontNumber {get; set;}
-    public string name {get; set;}
-    public decimal balance {get; set;}
-    public BankAccount(string name, decimal initialBalance){
+    private List<Transaction> allTransactions = new List<Transaction>();
+    public string accontNumber { get; set; }
+    public string name { get; set; }
+    public decimal balance
+    {
+        get
+        {
+            decimal amount = 0m;
+            foreach (var transaction in allTransactions)
+            {
+                amount += transaction.amount;
+            }
+            return amount;
+        }
+    }
+    public BankAccount(string name, decimal initialBalance)
+    {
         this.accontNumber = accountNumberSeed.ToString();
         accountNumberSeed++;
         this.name = name;
-        this.balance = initialBalance;
+        MakeDeposit(initialBalance, DateTime.Now, "Initial Deposit");
     }
-    public void MakeDeposit(decimal amount){
-
+    public void MakeDeposit(decimal amount, DateTime date, string note)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+        }
+        var deposit = new Transaction(amount, date, note);
+        allTransactions.Add(deposit);
     }
-    public void MakeWithdrawal(decimal amoung){
-
+    public void MakeWithdrawal(decimal amount, DateTime date, string note)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+        }
+        else if (balance - amount < 0)
+        {
+            throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+        }
+        var withdrawal = new Transaction(-amount, date, note);
+        allTransactions.Add(withdrawal);
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,4 +3,29 @@ using BankApplication;
 
 var account = new BankAccount("Arul", 5000m);
 
-Console.WriteLine($"Acoount no: {account.accontNumber}\nname: {account.name}\nBalance: {account.balance}");
+account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+Console.WriteLine(account.balance);
+account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+Console.WriteLine(account.balance);
+
+// Test for a negative initial balance
+BankAccount invalidAccount;
+try{
+    invalidAccount = new BankAccount("invalid", -55);
+}
+catch (ArgumentOutOfRangeException e)
+{
+    Console.WriteLine("Exception caught creating account with negative balance");
+    Console.WriteLine(e.ToString());
+}
+
+// Test for a negative balance.
+try
+{
+    account.MakeWithdrawal(7500, DateTime.Now, "Attempt to overdraw");
+}
+catch (InvalidOperationException e)
+{
+    Console.WriteLine("Exception caught trying to overdraw");
+    Console.WriteLine(e.ToString());
+}
